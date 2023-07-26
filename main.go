634011c4752ac2b1a9479b3de63c46dcb0d98eb1
Kamimura Teppei <teppei_kamimura@pkshatech.com>
2023-07-26 11:10:24 +0900

    main.go を作成しました。
    slackapi をたたいて1単語で検索ができることを確認しました。

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "net/http/httputil"
+)
+
+func main() {
+  // ref: https://qiita.com/taizo/items/c397dbfed7215969b0a5
+  url := "https://slack.com/api/search.messages?query=test&pretty=1"
+  // url := "https://slack.com/api/search.messages?query=AI&pretty=1"
+  req, _ := http.NewRequest("GET", url, nil)
+  req.Header.Set("Authorization", "Bearer xoxp-5634909965604-5632355661763-5656267501088-94f85e051307ec75eb82e0aa694666a5")
+
+  dump, _ := httputil.DumpRequestOut(req, true)
+  fmt.Printf("%s", dump)
+
+  client := new(http.Client)
+  resp, err := client.Do(req)
+  fmt.Printf("%s", err)
+
+  dumpResp, _ := httputil.DumpResponse(resp, true)
+  fmt.Printf("%s", dumpResp)
+}
